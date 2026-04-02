8111f84621cc45e89b34746d607ae1feeafa5efb
Fadel Oumar KOLOMA <fadel.koloma@infinity-africa.com>
2026-04-02 16:00:16 +0000

    version beta correctio

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000' 
+const BASE = import.meta.env.VITE_API_URL ||  'https://artci-backend.onrender.com/api'
 
 function getToken() {
   return localStorage.getItem('token')
